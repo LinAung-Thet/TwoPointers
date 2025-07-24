fdb9ddf3fbbd0383610b5340d49f4ccffa1024eb
LinAung-Thet <laungthet@gmail.com>
2025-07-24 17:20:56 +1200

    Modified CamelCaseMatching.cpp
    - Modified the logic to cater the test case 5.

--- CamelCaseMatching.cpp
+++ CamelCaseMatching.cpp
@@ -7,64 +7,37 @@ using namespace std;
 
 class Solution {
 public:
-    void split(string& str, vector<string>& output) {
-        if(str.size() < 1) return;
-
-        const char* s = str.c_str();
-        while(*s - 'a' >= 0 && *s != '\0') ++s; // Skip initial lowercase letters
-        if(*s == '\0') return; // If no uppercase letter found, return
-
-        while(*s != '\0') {
-            string substring;
-            do {
-                substring += *s++;
-            } while (*s - 'a' >= 0 && *s != '\0'); // Collect characters until an uppercase letter is found
-            output.push_back(substring);
-        }
-    }
     vector<bool> camelMatch(vector<string>& queries, string pattern) {
-        vector<bool> answers;
+        vector<bool> result;
 
-        // Split the pattern into sub-strings starting with a capital
-        vector<string> splitP;
-        split(pattern, splitP);
+        for (const string& query : queries) {
+            int i = 0, j = 0;
 
-        // Traverse each query in the queries
-        for(auto query: queries) {
-            
-            // Split the query into sub-strings starting with a capital
-            vector<string> splitQ;
-            split(query, splitQ);
-
-            // If the number of sub-strings are not the same, add false to the answer.
-            if(splitP.size() != splitQ.size()) {
-                answers.push_back(false);
-                continue;
-            }
-
-            // Compare all sub-strings with those of the pattern
-            bool ans = true;
-            for(int n = 0; n < splitP.size(); ++n) {
-
-                // Compare each sub-string by using two pointers
-                string sp = splitP[n], sq = splitQ[n];
-                const char* p = sp.c_str();
-                const char* q = sq.c_str();  
-                while(*p != '\0') {
-                    // while(*q != *p && *q != '\0') ++q;
-                    if(*q == *p) ++p;
-                    if(*q != '\0') ++q;
-                    else break;
-                }
-                if(*p != '\0') {
-                    ans = false;
+            while (i < query.size() && j < pattern.size()) {
+                if (query[i] == pattern[j]) {
+                    ++i;
+                    ++j;
+                } else if (islower(query[i])) {
+                    ++i;
+                } else {
                     break;
                 }
             }
 
-            answers.push_back(ans);
+            // Match is valid if entire pattern is consumed and remaining query chars are lowercase
+            bool isMatch = (j == pattern.size());
+            while (i < query.size()) {
+                if (isupper(query[i])) {
+                    isMatch = false;
+                    break;
+                }
+                ++i;
+            }
+
+            result.push_back(isMatch);
         }
-        return answers;
+
+        return result;
     }
 };
 // Test cases
@@ -74,53 +47,53 @@ int main() {
     string pattern;
     vector<bool> result;
 
-    // cout << "Test case 1" << endl;
-    // queries = {"FooBar", "FooBarTest", "FootBall", "FrameBuffer", "ForceFeedBack"};
-    // pattern = "FB";
-    // result = sol.camelMatch(queries, pattern);
-    // cout << "Pattern: " << pattern << endl;
-    // cout << "Expected: true false true true false" << endl;
-    // cout << "Result  : ";
-    // for (bool res : result) {
-    //     cout << (res ? "true" : "false") << " ";
-    // }
-    // cout << endl << endl;
+    cout << "Test case 1" << endl;
+    queries = {"FooBar", "FooBarTest", "FootBall", "FrameBuffer", "ForceFeedBack"};
+    pattern = "FB";
+    result = sol.camelMatch(queries, pattern);
+    cout << "Pattern: " << pattern << endl;
+    cout << "Expected: true false true true false" << endl;
+    cout << "Result  : ";
+    for (bool res : result) {
+        cout << (res ? "true" : "false") << " ";
+    }
+    cout << endl << endl;
 
-    // cout << "Test case 2" << endl;
-    // queries = {"FooBar", "FooBarTest", "FootBall", "FrameBuffer", "ForceFeedBack"};
-    // pattern = "FoBa";
-    // result = sol.camelMatch(queries, pattern);
-    // cout << "Pattern: " << pattern << endl;
-    // cout << "Expected: true false true false false" << endl;
-    // cout << "Result  : ";
-    // for (bool res : result) {
-    //     cout << (res ? "true" : "false") << " ";
-    // }
-    // cout << endl << endl;
+    cout << "Test case 2" << endl;
+    queries = {"FooBar", "FooBarTest", "FootBall", "FrameBuffer", "ForceFeedBack"};
+    pattern = "FoBa";
+    result = sol.camelMatch(queries, pattern);
+    cout << "Pattern: " << pattern << endl;
+    cout << "Expected: true false true false false" << endl;
+    cout << "Result  : ";
+    for (bool res : result) {
+        cout << (res ? "true" : "false") << " ";
+    }
+    cout << endl << endl;
 
-    // cout << "Test case 3" << endl;
-    // queries = {"FooBar", "FooBarTest", "FootBall", "FrameBuffer", "ForceFeedBack"};
-    // pattern = "FoBaT";
-    // result = sol.camelMatch(queries, pattern);
-    // cout << "Pattern: " << pattern << endl;
-    // cout << "Expected: false true false false false" << endl;
-    // cout << "Result  : ";
-    // for (bool res : result) {
-    //     cout << (res ? "true" : "false") << " ";
-    // }
-    // cout << endl << endl;
+    cout << "Test case 3" << endl;
+    queries = {"FooBar", "FooBarTest", "FootBall", "FrameBuffer", "ForceFeedBack"};
+    pattern = "FoBaT";
+    result = sol.camelMatch(queries, pattern);
+    cout << "Pattern: " << pattern << endl;
+    cout << "Expected: false true false false false" << endl;
+    cout << "Result  : ";
+    for (bool res : result) {
+        cout << (res ? "true" : "false") << " ";
+    }
+    cout << endl << endl;
 
-    // cout << "Test case 4" << endl;
-    // queries = {"uAxaqlzahfialcezsLfj","cAqlzyahaslccezssLfj","AqlezahjarflcezshLfj","AqlzofahaplcejuzsLfj","tAqlzahavslcezsLwzfj","AqlzahalcerrzsLpfonj","AqlzahalceaczdsosLfj","eAqlzbxahalcezelsLfj"};
-    // pattern = "AqlzahalcezsLfj";
-    // result = sol.camelMatch(queries, pattern);
-    // cout << "Pattern: " << pattern << endl;
-    // cout << "Expected: true true true true true true true true" << endl;
-    // cout << "Result  : ";
-    // for (bool res : result) {
-    //     cout << (res ? "true" : "false") << " ";
-    // }
-    // cout << endl << endl;
+    cout << "Test case 4" << endl;
+    queries = {"uAxaqlzahfialcezsLfj","cAqlzyahaslccezssLfj","AqlezahjarflcezshLfj","AqlzofahaplcejuzsLfj","tAqlzahavslcezsLwzfj","AqlzahalcerrzsLpfonj","AqlzahalceaczdsosLfj","eAqlzbxahalcezelsLfj"};
+    pattern = "AqlzahalcezsLfj";
+    result = sol.camelMatch(queries, pattern);
+    cout << "Pattern: " << pattern << endl;
+    cout << "Expected: true true true true true true true true" << endl;
+    cout << "Result  : ";
+    for (bool res : result) {
+        cout << (res ? "true" : "false") << " ";
+    }
+    cout << endl << endl;
 
 
     cout << "Test case 5" << endl;
